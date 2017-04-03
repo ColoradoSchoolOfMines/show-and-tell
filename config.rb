421a4fcf36b13da8e98e693d9c0bf44237f072e8
Sumner Evans <sumner.evans98@gmail.com>
2017-04-03 14:07:18 -0600

    added bootstrap

--- config.rb
+++ config.rb
@@ -1,8 +1,10 @@
 require 'compass/import-once/activate'
+require 'bootstrap-sass'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
+fonts_dir = "resources/fonts"
 css_dir = "resources/css"
 sass_dir = "resources/sass"
 images_dir = "resources/images"
@@ -10,6 +12,7 @@ javascripts_dir = "resources/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
