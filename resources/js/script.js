1177316672df68fcd1195b456f1bd2c2319dc0db
Sumner Evans <sumner.evans98@gmail.com>
2017-04-12 14:07:57 -0600

    styling stuff

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,33 +2,7 @@
  * script.js
  */
 
-var form_validator = function(form_cls) {
-    var form = $('#' + form_cls);
-
-    return {
-        validate: function() {
-            var descendants = form.find('div');
-            for (var i = 0; i < descendants.length; i++) {
-                var descendant = descendants[i];
-
-                if (descendant.classList.contains('required')) {
-                    var label = $(descendant).children('label');
-                    var field = $(descendant).children(label.attr('for'));
-                    console.log(field);
-
-                    if (field.validity.valid) {
-                        console.log('got here');
-                    }
-                }
-            }
-        }
-    };
-};
-
 $(document).ready(function() {
-    var fv = new form_validator('form');
-    fv.validate();
-
     // Go through all of the elements checking for data-collapse attributes
     $('*').each(function() {
         var button = $(this),
